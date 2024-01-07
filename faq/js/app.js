3439505359a6c9e048d6ed8b9c82ca95f276fbdc
abdurDiary <rahmansohel155@gmail.com>
2024-01-07 18:42:09 +0600

    abdur

---
+++ faq/js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  let qsn = $(".qsn");
+
+
+  qsn.on("click", function () {
+    let ans = $(".ans");
+       
+
+     if($(this)){
+
+     }
+
+    $(this).toggleClass("active");
+  });
+});
